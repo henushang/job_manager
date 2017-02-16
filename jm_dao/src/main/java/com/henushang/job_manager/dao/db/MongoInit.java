f2ba32547335d0f433592c6fccdb3823e3a24999
henushang <henushang@qq.com>
2017-02-17 00:58:53 +0800

    添加密码连接mongo方式

--- jm_dao/src/main/java/com/henushang/job_manager/dao/db/MongoInit.java
+++ jm_dao/src/main/java/com/henushang/job_manager/dao/db/MongoInit.java
@@ -1,5 +1,6 @@
 package com.henushang.job_manager.dao.db;
 
+import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
@@ -7,6 +8,8 @@ import org.apache.log4j.Logger;
 
 import com.henushang.job_manager.util.PropertiesUtil;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -27,12 +30,19 @@ public class MongoInit {
 		
 		String hostName = host.split(":")[0];
 		int port = Integer.parseInt(host.split(":")[1]);
-		mongo = new MongoClient(hostName, port);
-		logger.info("init MongoClient success!");
-		
 		dbName = map.get("mongo_db_name").toString();
 		checkEmpty(dbName, "dbName");
-		logger.info("config mongo_db_name value is :" + dbName);
+        logger.info("config mongo_db_name value is :"+ dbName);
+        String user = map.get("mongo_db_user").toString();
+        logger.info("user:" + user);
+        String pwd = map.get("mongo_db_pwd").toString();
+        logger.info("pwd:" + pwd);
+        String authDb = map.get("mongo_db_auth_db").toString();
+        MongoCredential credential = MongoCredential.createCredential(user, authDb, pwd.toCharArray());  
+		mongo = new MongoClient(new ServerAddress(host, port), Arrays.asList(new MongoCredential[]{credential}));
+		logger.info("init MongoClient success!");
+		
+		 
 	}
 
 	
